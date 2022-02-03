3ebd07160b6f7f248a0d1236a345ceb0a1d49585
Codatrix <ankur.codatrix@gmail.com>
2022-02-03 12:43:03 +0530

    express server bsic setup - basic imports completed - connection string to DB set - packages update completed - no errors found

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('E-Commerce API');
+// env vars
+require("dotenv").config();
+
+// express setup
+const express = require("express");
+const app = express();
+
+// import Connect to DB
+const connectDB = require("./db/connect");
+
+//--------------------------
+// Setting up the Server
+//--------------------------
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    // invoke connectDB
+    await connectDB(process.env.MONGO_URI);
+    // listen to the server
+    app.listen(port, (req, res) => {
+      console.log(`Server is running on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
