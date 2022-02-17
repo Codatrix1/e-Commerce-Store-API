b29ed6161c4770309e4fd12593229f7f6238ba9c
Codatrix <ankur.codatrix@gmail.com>
2022-02-17 20:57:35 +0530

    createReview function implemented

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,7 +1,16 @@
+// import models
 const Review = require("../models/reviewModel");
+const Product = require("../models/productModel");
+const User = require("../models/userModel");
+
+// rest of the imports
 const { StatusCodes } = require("http-status-codes");
 const CustomErrorAPI = require("../errors");
 
+// authentication imports
+const { checkPermissions } = require("../utils");
+
+//-----------------------------------------------
 // @ Desc      Get all reviews
 // @ Route     GET /api/v1/reviews
 // @ Access    Public
@@ -9,6 +18,7 @@ const getAllReviews = async (req, res, next) => {
   res.send("Get All Reviews");
 };
 
+//------------------------------------------------
 // @ Desc      Get single review
 // @ Route     GET /api/v1/reviews/:id
 // @ Access    Public
@@ -16,13 +26,42 @@ const getSingleReview = async (req, res, next) => {
   res.send("Get Single Review");
 };
 
+//------------------------------------------------
 // @ Desc      Create review
 // @ Route     POST /api/v1/reviews
 // @ Access    Private
 const createReview = async (req, res, next) => {
-  res.send("Create Review");
+  // 1) destructure the incoming request , check for productId and manually re-assign values
+  const { product: productId } = req.body;
+
+  // 2) check if the product exists in the DB
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new CustomErrorAPI.NotFoundError(
+      `No product found with the ID of ${productId}`
+    );
+  }
+  // 3) WE check the role as "user" OR "admin" in our cookie and then proceed with the below code
+  // Manually re-assign the property coming from req.body attaching a user to it
+  req.body.user = req.user.userId;
+
+  // 4) Check if the "current logged in user" has already left a "review" on the "specific product"
+  const alreadyReviewed = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadyReviewed) {
+    throw new CustomErrorAPI.BadRequestError(
+      "Already submitted a review for this product"
+    );
+  }
+
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
+//-------------------------------------------------
 // @ Desc      Update review
 // @ Route     PATCH /api/v1/reviews/:id
 // @ Access    Private
@@ -30,6 +69,7 @@ const updateReview = async (req, res, next) => {
   res.send("Update Review");
 };
 
+//-------------------------------------------------
 // @ Desc      Delete review
 // @ Route     Delete /api/v1/reviews/:id
 // @ Access    Private
