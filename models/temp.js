97690b5b3d7f454da932b1e71b647bd684e09a4f
Codatrix <ankur.codatrix@gmail.com>
2022-02-20 20:16:02 +0530

    Aggregation pipeline code snippet compiled using MongoDB Atlas/Compass and exported with node syntax - saved the snippet temporarily in temp.js

---
+++ models/temp.js
@@ -0,0 +1,36 @@
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId("62124ddc95e585224fef8cec"),
+    },
+  },
+  {
+    $group: {
+      _id: "$product",
+      averageRating: {
+        $avg: "$rating",
+      },
+      numOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+MongoClient.connect(
+  " ",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (connectErr, client) {
+    assert.equal(null, connectErr);
+    const coll = client.db("10-E-COMMERCE-API").collection("reviews");
+    coll.aggregate(agg, (cmdErr, result) => {
+      assert.equal(null, cmdErr);
+    });
+    client.close();
+  }
+);
